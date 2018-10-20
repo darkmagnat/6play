ace54199b2620db961db273d189fc292f04fbde4
darkmagnat <43116040+darkmagnat@users.noreply.github.com>
2018-10-20 19:34:17 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -20,7 +20,7 @@ const gif = require("gif-search");
 
 const client = new Discord.Client({disableEveryone: true});
 
-const prefix = "6";
+const prefix = "?";
 /////////////////////////
 ////////////////////////
 
@@ -313,10 +313,10 @@ function play(guild, song) {
 
 
 client.on('message', message => {
-    if (message.content === '6help') {
+    if (message.content === '?help') {
         let helpEmbed = new Discord.RichEmbed()
         .setTitle('**أوامر الميوزك...**')
-        .setDescription('**(6) برفكس البوت **')
+        .setDescription('**(?) برفكس البوت **')
         .addField('play', 'لتشغيل اغنية')
         .addField('join', 'دخول رومك الصوتي')
         .addField('disconnect', 'الخروج من رومك الصوتي')
@@ -349,7 +349,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : Diamond Codes ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`Venom Network`,"http://twitch.tv/S-F")
+client.user.setGame(`RoBx, | 1K"Sooon`,"http://twitch.tv/S-F")
 client.user.setStatus("online")
  
 });
